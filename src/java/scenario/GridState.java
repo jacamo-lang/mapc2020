c38eea46aa3f3fc967104d1006d75025b5e2e15c
cleberjamaral <cleberjamaral@gmail.com>
2020-06-25 01:20:32 -0300

    Best path ignoring close agents and far obstacles

--- src/java/scenario/GridState.java
+++ src/java/scenario/GridState.java
@@ -17,7 +17,7 @@ public class GridState implements Estado, Heuristica {
     private Table<Integer, Integer, String> map;
 
     /**
-     * 
+     *
      * @param l
      * @param from
      * @param to
@@ -60,9 +60,9 @@ public class GridState implements Estado, Heuristica {
     private void suc(List<Estado> s, Location newl, String direction) {
         // Avoiding obstacles and agents of both teams
         if (    (!map.contains(newl.x, newl.y)) || (
-                (!map.get(newl.x, newl.y).equals("a")) && // an agent of team a
-                (!map.get(newl.x, newl.y).equals("b")) && // an agent of team b
-                (!map.get(newl.x, newl.y).equals("obstacle"))
+                (!map.get(newl.x, newl.y).equals("a") || euclidianDistance(from,newl) > 3) && // an agent of team a
+                (!map.get(newl.x, newl.y).equals("b") || euclidianDistance(from,newl) > 3) && // an agent of team b
+                (!map.get(newl.x, newl.y).equals("obstacle") || euclidianDistance(from,newl) > 10)
                 )) {
             s.add(new GridState(newl, from, to, direction, map));
         }
@@ -76,6 +76,10 @@ public class GridState implements Estado, Heuristica {
         return false;
     }
 
+    public int euclidianDistance(Location from, Location to) {
+        return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
+    }
+
     public int hashCode() {
         return pos.hashCode();
     }
@@ -83,7 +87,7 @@ public class GridState implements Estado, Heuristica {
     public String toString() {
         return "(" + pos + "-" + direction + ")";
     }
-    
+
     public String getDirection() {
         return direction;
     }
