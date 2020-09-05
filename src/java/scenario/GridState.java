e393873d31e049c9981da61ea8af988ad24b03f7
cleberjamaral <cleberjamaral@gmail.com>
2020-09-04 21:10:50 -0300

    Removed redundant euclideanDistance(from,to)

--- src/java/scenario/GridState.java
+++ src/java/scenario/GridState.java
@@ -60,9 +60,9 @@ public class GridState implements Estado, Heuristica {
     private void suc(List<Estado> s, Location newl, String direction) {
         // Avoiding obstacles and agents of both teams
         if (    (!map.contains(newl.x, newl.y)) || (
-                (!map.get(newl.x, newl.y).equals("a") || euclideanDistance(from,newl) > 3) && // an agent of team a
-                (!map.get(newl.x, newl.y).equals("b") || euclideanDistance(from,newl) > 3) && // an agent of team b
-                (!map.get(newl.x, newl.y).equals("obstacle") || euclideanDistance(from,newl) > 10)
+                (!map.get(newl.x, newl.y).equals("a") || from.distance(newl) > 3) && // an agent of team a
+                (!map.get(newl.x, newl.y).equals("b") || from.distance(newl) > 3) && // an agent of team b
+                (!map.get(newl.x, newl.y).equals("obstacle") || from.distance(newl) > 10)
                 )) {
             s.add(new GridState(newl, from, to, direction, map));
         }
@@ -74,10 +74,6 @@ public class GridState implements Estado, Heuristica {
             return pos.equals(m.pos);
         } catch (Exception e) {}
         return false;
-    }
-
-    public int euclideanDistance(Location from, Location to) {
-        return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
     }
 
     public int hashCode() {
